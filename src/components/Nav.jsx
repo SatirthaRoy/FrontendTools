2b29aecfd9637ccd06ffc4eca825681d3abbed18
himu <satirtharoy2003@gmail.com>
2024-03-18 20:22:16 +0600

    added gradient blur using mask-image in index.css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,13 +3,16 @@ import frontend from '../assets/SVGS/FronTend ToolsFT.svg'
 
 const Nav = () => {
   return (
-    <nav className='flex justify-between items-center mt-6 py-6 w-11/12 mx-auto sticky top-0'>
-      <img src={frontend} alt="" />
-      <h3 className='text-[#F9F7FA] font-semibold text-xl cursor-pointer'>TOOLS
-        <div className='bg-white h-1 w-full'></div>
-      </h3>
-      <button className='text-white font-semibold text-xl bg-transparent hover:bg-white border hover:text-[#272328] py-4 md:px-16 border-white'>CONTACT</button>
-    </nav>
+    <>
+      <div id='glossy' className='z-40'></div>
+      <nav className='flex justify-between items-center mt-6 py-6 w-11/12 mx-auto sticky top-0 z-50'>
+        <img src={frontend} alt="" />
+        <h3 className='text-[#F9F7FA] font-semibold text-xl cursor-pointer'>TOOLS
+          <div className='bg-white h-1 w-full'></div>
+        </h3>
+        <button className='text-white font-semibold text-xl bg-transparent hover:bg-white border hover:text-[#272328] py-4 md:px-16 border-white'>CONTACT</button>
+      </nav>
+    </>
   )
 }
 
