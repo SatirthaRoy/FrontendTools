5863a0337dda3ab79b0938b4ef4c2920e7bd569f
himu <satirtharoy2003@gmail.com>
2024-03-18 12:01:40 +0600

    project started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+      <div className=" bg-black top-0 w-screen h-screen flex justify-center items-center overflow-hidden">
+        <div className=" size-[560px] bg-white rounded-full blur-[250px]"> manger beta</div>
+      </div>
+  )
+}
+
+export default App
